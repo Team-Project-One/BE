ad18925c9ea89975c8955d7e9c94cb1e709541a1
audwns03 <myeongjunkang@gmail.com>
2025-11-22 02:12:38 +0900

    feat : 오늘의 운세 캐시 기능
    -오늘의 운세 캐시 저장 + 하루별로 초기화

---
+++ src/main/java/project/backend/config/CacheConfig.java
@@ -0,0 +1,21 @@
+package project.backend.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+
+        cacheManager.setCaffeine(Caffeine.newBuilder().maximumSize(100));
+        cacheManager.setCacheNames(java.util.Collections.singletonList("fortunes"));
+
+        return cacheManager;
+    }
+}
--- src/main/java/project/backend/fortune/FortuneService.java
+++ src/main/java/project/backend/fortune/FortuneService.java
@@ -1,6 +1,7 @@
 package project.backend.fortune;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import project.backend.fortune.dto.FortuneDTO;
 import project.backend.openai.OpenAiService;
@@ -11,6 +12,7 @@ public class FortuneService {
 
     private final OpenAiService openAiService;
 
+    @Cacheable(value = "fortunes" ,key = "T(java.time.LocalDate).now().toString()")
     public FortuneDTO getTodayFortune() {
         return openAiService.getTodayFortune();
     }
