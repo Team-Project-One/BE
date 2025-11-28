3ab11f4ba26a9eaab89616332ae204fd257bb00a
강명준 <myeongjunkang@gmail.com>
2025-11-28 15:55:08 +0900

    AuthController 앱기반 수정 (#4)
    Co-authored-by: lsryl13578 <tkdfuf1008@gmail.com>

--- src/main/java/project/backend/kakaoLogin/AuthController.java
+++ src/main/java/project/backend/kakaoLogin/AuthController.java
@@ -1,14 +1,40 @@
 package project.backend.kakaoLogin;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "auth-controller", description = "카카오 로그인")
+public class AuthController {
+
+    private final KakaoOAuthService kakaoOAuthService;
+
+    @GetMapping("/kakao/callback")
+    public RedirectView kakaoCallback(@RequestParam("code") String code) {
+
+        JwtTokenResponse tokens = kakaoOAuthService.loginWithKakao(code);
+
+        // 딥링크 URL 구성 (앱으로 반환)
+        String redirectUrl = String.format(
+                "divineapp://auth/kakao/callback?accessToken=%s&refreshToken=%s&newUser=%s",
+                tokens.getAccessToken(),
+                tokens.getRefreshToken(),
+                tokens.isNewUser()
+        );
+
+        return new RedirectView(redirectUrl);
+    }
+}
+
+/*
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -23,3 +49,4 @@ public class AuthController {
         return ResponseEntity.ok(tokens);
     }
 }
+*/
